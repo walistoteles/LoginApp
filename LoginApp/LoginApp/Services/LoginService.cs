41a125f8382587a5e881aa1fdfcd9cda74cf0af9
Wali <Wali@DESKTOP-36JIRUG>
2021-10-20 17:49:40 -0300

    User app att

---
+++ LoginApp/LoginApp/Services/LoginService.cs
@@ -0,0 +1,18 @@
+﻿using LoginApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Services
+{
+    public class LoginService
+    {
+
+      
+
+
+
+
+    }
+}
---
+++ LoginApp/LoginApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,98 @@
+﻿using LoginApp.Models;
+using LoginApp.Services;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LoginApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+
+        public LoginPage()
+        {
+            InitializeComponent();
+
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            string json = "";
+
+            List<User> user = new List<User>();
+
+
+            try
+            {
+                json = await Xamarin.Essentials.SecureStorage.GetAsync("user");
+
+
+
+                user = JsonConvert.DeserializeObject<List<User>>(json);
+
+                for (int i = 0; i < user.Count; i++)
+                {
+                    if(Password.Text == user[i].password)
+                    {
+                        //senha correta!
+                        await DisplayAlert("Login Feito", "Login Feito com Sucesso", "OK");
+
+                        break;
+                    }
+
+                    //senha incorreta!
+                    await DisplayAlert("Error", "Senha Incorreta", "OK");
+                    break;
+                    
+                }
+            }
+            catch 
+            {
+
+                json = "nothing yet";
+            }
+
+
+
+
+        }
+
+
+        async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            //register
+            List<User> userlist = new List<User>();
+            User user = new User();
+
+            if(string.IsNullOrEmpty(await Xamarin.Essentials.SecureStorage.GetAsync("user")))
+            {
+                // VAZIO
+            }
+            else
+            {
+                string jsonlist = await Xamarin.Essentials.SecureStorage.GetAsync("user");
+                userlist = JsonConvert.DeserializeObject<List<User>>(jsonlist);
+            }
+          
+
+
+
+            user.password = Password.Text;
+            user.email = Email.Text;
+
+
+            userlist.Add(user);
+
+            string jsonuser = JsonConvert.SerializeObject(userlist);
+
+            await Xamarin.Essentials.SecureStorage.SetAsync("user", jsonuser);
+        }
+    }
+}
